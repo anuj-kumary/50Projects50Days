4cf3942b33aa577186a8fa107781c50bc1e2a240
anuj-kumary <anujf0510@gmail.com>
2021-06-23 21:24:49 +0530

    fix : bug

--- Login-Modal/index.js
+++ Login-Modal/index.js
@@ -1,20 +1,11 @@
 const openModal = document.getElementById('btn')
-const btnHide = document.getElementById('modal-btn')
 const modalData = document.getElementById('modal-data')
 const closeModal = document.getElementById('close')
 
 openModal.addEventListener('click',function(){
-    modalData.style.display = "block"
-    btnHide.style.display = "none"
+    modalData.style.display = "block";
 })
 
 closeModal.addEventListener('click',function(){
     modalData.style.display = "none";
-    btnHide.style.display = "block"
-})
-
-window.addEventListener('click',function(e){
-    if(e.target == modalData){
-        modalData.style.display = "none";
-    }
 })
