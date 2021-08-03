8ef1284e931b239b0257510e03791f666ac14cec
anuj-kumary <anujf0510@gmail.com>
2021-08-03 17:31:06 +0530

    feat : getting data from Api

--- Weather-Forecast/index.js
+++ Weather-Forecast/index.js
@@ -1,15 +1,26 @@
 let userValue = document.getElementById('btn')
-
+let userInput = document.getElementById('user-value')
 
 let apiKey = 'd456a47b83d74660dc1fe4eb311b01c4';
-let cityName = 'mumbai';
+let cityName = "";
 
-fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`)
+userInput.addEventListener('change',function(e){
+    cityName = e.target.value
+    console.log(cityName)
+fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d456a47b83d74660dc1fe4eb311b01c4`)
 .then( (data) => {
     return data.json()
 })
 .then( (weather) =>{
-    console.log(weather)
+    console.log(weather.name)
+    console.log(weather.main.temp)
+    // console.log(weather)
+   console.log( `${Math.floor(weather.main.temp - 273.15)}&deg`)
 }).catch( (err) =>{
     console.log(err)
 })
+    
+})
+
+
+
