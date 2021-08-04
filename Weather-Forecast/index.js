fce5060c690a0ad6c68562b1b52510935394c47f
anuj-kumary <anujf0510@gmail.com>
2021-08-04 14:08:26 +0530

    style : responsivness

--- Weather-Forecast/index.js
+++ Weather-Forecast/index.js
@@ -1,31 +1,38 @@
 let userValue = document.getElementById('btn')
-let temperature = document.getElementById('temperature')
+let getTemperature = document.getElementById('temperature')
 let userInput = document.getElementById('user-value')
-let cityname = document.getElementById('city-name')
-let cloudy = document.getElementById('cloudy')
-let icon = document.getElementById('icon')
+let getCityname = document.getElementById('city-name')
+let getDescription = document.getElementById('cloudy')
+let getIcon = document.querySelector('.icon')
+let dateTime = document.querySelector('#time-date')
+
+
+setInterval(()=>{
+    let Dates = new Date();
+    dateTime.innerHTML = `${Dates.toDateString()}, ${Dates.toLocaleTimeString('en-US')}`
+},1000)
+
 
 let apiKey = 'd456a47b83d74660dc1fe4eb311b01c4';
 let cityName = "";
 
 userInput.addEventListener('change',function(e){
     cityName = e.target.value
-    console.log(cityName)
 fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d456a47b83d74660dc1fe4eb311b01c4`)
-.then( (data) => {
-    return data.json()
+.then( (res) => {
+    return res.json()
 })
-.then( (weather) =>{
-    console.log(weather.name)
-    console.log(weather)
-    temperature.innerHTML =  `${Math.floor(weather.main.temp - 273.15)} &degC`;
-    cityname.innerHTML = weather.name;
-    cloudy.innerHTML = weather.weather[0].description;
+.then( (data) =>{
+    getTemperature.innerHTML =  `${Math.floor(data.main.temp - 273.15)} &degC`;
+    getCityname.innerHTML = data.name;
+    getDescription.innerHTML = data.weather[0].description;
+    getIcon.innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png"/>`
 }).catch( (err) =>{
     console.log(err)
 })
     
 })
+
 
 
 
