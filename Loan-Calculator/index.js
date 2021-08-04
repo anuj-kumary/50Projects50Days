660f1bf11764a1d3b73ab37011eec1950814b83a
anuj-kumary <anujf0510@gmail.com>
2021-08-04 22:50:15 +0530

    feat : loan Calculator

---
+++ Loan-Calculator/index.js
@@ -0,0 +1,22 @@
+const calculateBtn = document.getElementById('calculate-btn')
+const result = document.querySelector('.result')
+
+calculateBtn.addEventListener('click',function(e){
+   e.preventDefault();
+
+    var loanAmount = document.getElementById('amount').value;
+    let getInterest = document.getElementById('interest').value;
+    let getMonths = document.getElementById('months').value;
+    
+    const interest = (loanAmount * (getInterest * 0.01)) / getMonths;
+    const total = ((loanAmount / getMonths) + interest);
+    const totalInterest = (total * getMonths - loanAmount)
+    const totalPayment = (total * getMonths);
+    
+    result.style.display = "block"
+    document.querySelector('.monthly-payment').innerHTML = "Monthly Payment &#x7c; " +total;
+    document.querySelector('.interest').innerHTML = "Total Interest " +totalInterest;
+    document.querySelector('.total-payment').innerHTML ="Total Payment " +totalPayment
+
+
+})
